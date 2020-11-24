ebf831cc3bfd519c89e55d48e76e84e514b8bf54
Alejandro Larumbe <larumbemar@gmail.com>
2020-11-25 12:30:48 +1300

    initial commit

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
+import Splash from "./components/splash/Splash"
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
@@ -28,25 +29,27 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar setAuthenticated={setAuthenticated} />
-      <Route path="/login" exact={true}>
-        <LoginForm
-          authenticated={authenticated}
-          setAuthenticated={setAuthenticated}
-        />
-      </Route>
-      <Route path="/sign-up" exact={true}>
-        <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
-      </Route>
-      <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-        <UsersList/>
-      </ProtectedRoute>
-      <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
-        <User />
-      </ProtectedRoute>
-      <ProtectedRoute path="/" exact={true} authenticated={authenticated}>
-        <h1>My Home Page</h1>
-      </ProtectedRoute>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Splash>Splash</Splash>
+        </Route>
+        <NavBar setAuthenticated={setAuthenticated} />
+        <Route path="/login" exact={true}>
+          <LoginForm
+            authenticated={authenticated}
+            setAuthenticated={setAuthenticated}
+          />
+        </Route>
+        <Route path="/sign-up" exact={true}>
+          <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
+        </Route>
+        <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
+          <UsersList/>
+        </ProtectedRoute>
+        <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
+          <User />
+        </ProtectedRoute>
+      </Switch>
     </BrowserRouter>
   );
 }
---
+++ react-app/src/components/splash/Splash.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+
+const Splash = (props) => {
+  /* Should render:
+    header(with signin/create accoun buttons),
+    banner
+    div with app info
+    component search bar
+    component featured tracks
+    component categories
+    component sign up
+    component featured artists
+    component footer with all y'alls info
+  */
+  return (
+    <h1>Splash</h1>
+  )
+}
+
+const SplashContainer = () => {
+
+
+  return (
+    <Splash
+    />
+  )
+}
+
+export default SplashContainer
