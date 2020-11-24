16f61eaac2d0c331b691d6f8873b100a9de60d8b
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-11-24 08:08:48 -0800

    models

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -6,7 +6,7 @@ class Transaction(db.Model):
     __tablename__ = 'transactions'
 
     id = Column(db.Integer, primary_key=True)
-    amount = Column(db.Integer, default=50, nullable=False)
+    amount = Column(db.Integer, nullable=False)
     sender_id = Column(db.Integer, ForeignKey("artists.id"), nullable=False)
     recipient_id = Column(db.Integer, ForeignKey("artists.id"), nullable=False)
 
