0be46a36d3bf21a4dde2d78b741dee1113d37b39
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-11-25 17:03:17 -0800

    Fixed login/logout

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { logout } from "../../services/auth";
+import { useHistory } from 'react-router-dom';
 
-const LogoutButton = ({setAuthenticated}) => {
+const LogoutButton = ({ setAuthenticated }) => {
+  let history = useHistory();
   const onLogout = async (e) => {
     await logout();
     setAuthenticated(false);
+    history.push("/splash")
   };
 
   return <button onClick={onLogout}>Logout</button>;
