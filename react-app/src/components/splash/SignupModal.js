392323b64388dc0162f46751b4e4439ea05f45c5
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-11-25 18:16:32 -0800

    Sign up/login working.

--- react-app/src/components/splash/SignupModal.js
+++ react-app/src/components/splash/SignupModal.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../services/auth';
+import { Redirect, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+// import { signUp } from '../../services/auth';
+import { signupUser } from '../../store/actions/signupActions';
 import Modal from 'react-modal';
 import "./modal.css";
 
 const SignupModal = ({authenticated, setAuthenticated}) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [bio, setBio] = useState("");
@@ -17,17 +22,26 @@ const SignupModal = ({authenticated, setAuthenticated}) => {
   const [repeatPassword, setRepeatPassword] = useState("");
   const [modalIsOpen, setIsOpen] = useState(false);
 
-  if (authenticated) {
-    return <Redirect to="/" />;
-  }
-
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
-      const user = await signUp(name,username,email,bio,country,city,profileImageUrl,coverImageUrl,password);
-      setIsOpen(false);
+      let user = new FormData();
+      console.log("user-before:", user)
+      user.append('name', name);
+      user.append('username', username);
+      user.append('bio', bio);
+      user.append('country', country);
+      user.append('city', city);
+      user.append('profileImage', profileImageUrl);
+      user.append('coverImage', coverImageUrl);
+      user.append('email', email);
+      user.append('password', password);
+      console.log("user:", user)
+      user = await dispatch(signupUser(user));
+
       if (!user.errors) {
         setAuthenticated(true);
+        history.push("/home")
       }
     }
   };
@@ -52,11 +66,11 @@ const SignupModal = ({authenticated, setAuthenticated}) => {
     setCity(e.target.value);
   };
   const updateProfileImageUrl = (e) => {
-    setProfileImageUrl(e.target.value);
+    setProfileImageUrl(e.target.files[0]);
   };
 
   const updateCoverImageUrl = (e) => {
-    setCoverImageUrl(e.target.value);
+    setCoverImageUrl(e.target.files[0]);
   };
 
   const updateEmail = (e) => {
@@ -88,100 +102,99 @@ const SignupModal = ({authenticated, setAuthenticated}) => {
         </div>
 
         <form onSubmit={onSignUp}>
-        <div className="login-content">
-          <label>Name</label>
-          <input
-            type="text"
-            name="name"
-            onChange={updateName}
-            value={name}
-          ></input>
-        </div>
-        <div className="login-content">
-          <label>Username</label>
-          <input
-            type="text"
-            name="username"
-            onChange={updateUsername}
-            value={username}
-          ></input>
-        </div>
-        <div className="login-content">
-          <label>Email</label>
-          <input
-            type="text"
-            name="email"
-            onChange={updateEmail}
-            value={email}
-          ></input>
-        </div>
-        <div className="login-content">
-          <label>Bio</label>
-          <input
-            type="text"
-            name="bio"
-            onChange={updateBio}
-            value={bio}
-          ></input>
-        </div>
-        <div className="login-content">
-          <label>Country</label>
-          <input
-            type="text"
-            name="country"
-            onChange={updateCountry}
-            value={country}
-          ></input>
-        </div>
-        <div className="login-content">
-          <label>City</label>
-          <input
-            type="text"
-            name="city"
-            onChange={updateCity}
-            value={city}
-          ></input>
-        </div>
-        <div className="login-content">
-          <label>Profile Image</label>
-          <input
-            type="text"
-            name="profile_image"
-            onChange={updateProfileImageUrl}
-            value={profileImageUrl}
-          ></input>
-        </div>
-        <div className="login-content">
-          <label>Cover Image</label>
-          <input
-            type="text"
-            name="cover_image"
-            onChange={updateCoverImageUrl}
-            value={coverImageUrl}
-          ></input>
-        </div>
-        <div className="login-content">
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            onChange={updatePassword}
-            value={password}
-          ></input>
-        </div>
-        <div className="login-content">
-          <label>Repeat Password</label>
-          <input
-            type="password"
-            name="repeat_password"
-            onChange={updateRepeatPassword}
-            value={repeatPassword}
-            required={true}
-          ></input>
-        </div>
-        <div className="login-content">
-          <button type="submit">Sign Up</button>
-        </div>
+
+          <div className="login-content">
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              onChange={updateName}
+              value={name}
+            ></input>
+          </div>
+          <div className="login-content">
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              onChange={updateUsername}
+              value={username}
+            ></input>
+          </div>
+          <div className="login-content">
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              onChange={updateEmail}
+              value={email}
+            ></input>
+          </div>
+          <div className="login-content">
+            <label>Bio</label>
+            <input
+              type="text"
+              name="bio"
+              onChange={updateBio}
+              value={bio}
+            ></input>
+          </div>
+          <div className="login-content">
+            <label>Country</label>
+            <input
+              type="text"
+              name="country"
+              onChange={updateCountry}
+              value={country}
+            ></input>
+          </div>
+          <div className="login-content">
+            <label>City</label>
+            <input
+              type="text"
+              name="city"
+              onChange={updateCity}
+              value={city}
+            ></input>
+          </div>
+          <div className="login-content">
+            <label>Profile Image</label>
+            <input
+              type="file"
+              name="profile_image"
+              onChange={updateProfileImageUrl}
+            ></input>
+          </div>
+          <div className="login-content">
+            <label>Cover Image</label>
+            <input
+              type="file"
+              name="cover_image"
+              onChange={updateCoverImageUrl}
+            ></input>
+          </div>
+          <div className="login-content">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              onChange={updatePassword}
+              value={password}
+            ></input>
+          </div>
+          <div className="login-content">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              name="repeat_password"
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              required={true}
+            ></input>
+          </div>
+          <div className="login-content">
+            <button type="submit">Sign Up</button>
+          </div>
         </form>
       </Modal>
     </div>
