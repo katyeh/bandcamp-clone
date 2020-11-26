c83da9845fce71fe6eb4539f886f3124320c189e
Alejandro Larumbe <larumbemar@gmail.com>
2020-11-27 11:00:55 +1300

    useaudio

--- react-app/src/components/player/Controls.js
+++ react-app/src/components/player/Controls.js
@@ -1,21 +1,21 @@
-import React from 'react';
+// import React from 'react';
 
 
-const Controls = ({isPlaying, setIsPlaying, skipTrack}) => {
+// const Controls = ({isPlaying, setIsPlaying, skipTrack}) => {
 
-  return (
-    <div style={style}>
-      <i className="fa fa-fast-backward" onClick={() => skipTrack(true)}></i>
-      <i className={!isPlaying?"fa fa-play":"fas fa-pause"} onClick={() => setIsPlaying(!isPlaying)}></i>
-      <i className="fa fa-fast-forward" onClick={() => skipTrack(false)}></i>
-    </div>
-  )
-}
+//   return (
+//     <div style={style}>
+//       <i className="fa fa-fast-backward" onClick={() => skipTrack(true)}></i>
+//       <i className={!isPlaying?"fa fa-play":"fas fa-pause"} onClick={() => setIsPlaying(!isPlaying)}></i>
+//       <i className="fa fa-fast-forward" onClick={() => skipTrack(false)}></i>
+//     </div>
+//   )
+// }
 
-let style = {
-  color:'white',
-  display: "flex",
-}
+// let style = {
+//   color:'white',
+//   display: "flex",
+// }
 
 
-export default Controls
+// export default Controls
--- react-app/src/components/player/Player.js
+++ react-app/src/components/player/Player.js
@@ -3,117 +3,119 @@ import Controls from './Controls'
 
 
 
-function Player({ currentTrackIndex, setCurrentTrackIndex, tracks }) {
-  const audioEl = useRef(null)
-  const [isPlaying, setIsPlaying] = useState(false);
+// function Player({ currentTrackIndex, setCurrentTrackIndex, tracks }) {
+
+function useAudio( url ) {
+
+  const audioRef = useRef(null)
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isSeeking, setIsSeeking] = useState(false)
-
+  const [playbackStatus, setPlaybackStatus] = useState('pause');
+  const [isLoading, setLoading] = useState(true);
+  const [isSeeking, setSeeking] = useState(false)
 
   useEffect(() => {
-    if (isPlaying) {
-      audioEl.current.play();
-      console.log(audioEl)
-    } else {
-      audioEl.current.pause();
-    }
-  })
+    setLoading(true);
+  }, [url])
 
-
-
-  const skipTrack = (forwards = true) => {
-    if (forwards) {
-      setCurrentTrackIndex(() => {
-        let idx = currentTrackIndex;
-        idx++
-
-        if (idx > tracks.length - 1) {
-          idx = 0
-        }
-        return idx
-      });
-    } else {
-      setCurrentTrackIndex(() => {
-        let idx = currentTrackIndex;
-        idx--;
-
-        if (idx < 0) {
-          idx = tracks.length - 1
-        }
-        return idx
-      })
-    }
-  }
-  return (
-    <div style={style}>
-      <audio src={tracks[currentTrackIndex].mp3_url} ref={audioEl}></audio>
-      <h4>Playing now</h4>
-      <Controls
-        isPlaying={isPlaying}
-        setIsPlaying={setIsPlaying}
-        skipTrack={skipTrack}
-      />
-    </div>
-  )
-}
-
-const PlayerContainer = (props) => {
-  const [tracks, setTracks] = useState([
-    {
-      'title': 'Best I Ever Head',
-      'mp3_url': 'https://busker2.s3.amazonaws.com/songs/drake/Best+I+Ever+Had.mp3',
-      'lyrics': '<<URL HERE>>',
-      "album_id": 6,
-      "artist_id": 2
-    },
-    {
-      'title': 'Farandulera',
-      'mp3_url': 'https://busker2.s3.amazonaws.com/songs/maluma/Farandulera+Maluma+Letra.mp3',
-      'lyrics': '<<URL HERE>>',
-      'album_id': 7,
-      'artist_id': 5
-    },
-    {
-      'title': 'vamos a pasarla bien',
-      'mp3_url': 'https://busker2.s3.amazonaws.com/songs/maluma/vamos+a+pasarla+bien+maluma+letra.mp3',
-      'lyrics': '<<URL HERE>>',
-      'album_id': 8,
-      'artist_id': 5
-    },
-    {
-      'title': 'Beautiful, Dirty, Rich',
-      'mp3_url': 'https://busker2.s3.amazonaws.com/songs/ladygaga/Lady+Gaga+-+Beautiful%2C+Dirty%2C+Rich.mp3',
-      'lyrics': '<<URL HERE>>',
-      'album_id': 9,
-      'artist_id': 6
-    }
-  ])
-  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
 
 
   return (
-    <Player
-    currentTrackIndex={currentTrackIndex}
-    setCurrentTrackIndex={setCurrentTrackIndex}
-    tracks={tracks}
-    />
+      <audio
+        onLoadedData={() => {
+          setPlaybackStatus('pause');
+          setLoading(false);
+          setDuration(audioRef.current.duration);
+        }}
+        onSeeking={() => setSeeking(true)}
+        onSeeked={() => setSeeking(false)}
+        src={url}
+        ref={audioRef}
+        onTimeUpdate={() => {
+          setCurrentTime(audioRef.current.currentTime);
+        }}
+        hidden
+      />,
+      {
+        currentTime,
+        duration,
+        playbackStatus,
+        isSeeking,
+        isLoading,
+        progress: (currentTime / duration) * 100,
+        setTime: seconds => {
+            audioRef.current.currentTime = seconds;
+        },
+        togglePlaybackStatus: () => {
+          if (playbackStatus === 'play') {
+            audioRef.current.pause();
+            setPlaybackStatus('pause');
+          }
+          if (playbackStatus === 'pause') {
+            audioRef.current.play()
+          }
+
+        }
+      }
+
   )
 }
 
-let style = {
-  position: "absolute",
-  bottom: "0",
-  left: "0",
-  width: "100%",
-  height: "80px",
-  background: "#282828",
-  // zIndex: "99",
-  padding: "0 20px",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}
+// const PlayerContainer = (props) => {
+//   const [tracks, setTracks] = useState([
+//     {
+//       'title': 'Best I Ever Head',
+//       'mp3_url': 'https://busker2.s3.amazonaws.com/songs/drake/Best+I+Ever+Had.mp3',
+//       'lyrics': '<<URL HERE>>',
+//       "album_id": 6,
+//       "artist_id": 2
+//     },
+//     {
+//       'title': 'Farandulera',
+//       'mp3_url': 'https://busker2.s3.amazonaws.com/songs/maluma/Farandulera+Maluma+Letra.mp3',
+//       'lyrics': '<<URL HERE>>',
+//       'album_id': 7,
+//       'artist_id': 5
+//     },
+//     {
+//       'title': 'vamos a pasarla bien',
+//       'mp3_url': 'https://busker2.s3.amazonaws.com/songs/maluma/vamos+a+pasarla+bien+maluma+letra.mp3',
+//       'lyrics': '<<URL HERE>>',
+//       'album_id': 8,
+//       'artist_id': 5
+//     },
+//     {
+//       'title': 'Beautiful, Dirty, Rich',
+//       'mp3_url': 'https://busker2.s3.amazonaws.com/songs/ladygaga/Lady+Gaga+-+Beautiful%2C+Dirty%2C+Rich.mp3',
+//       'lyrics': '<<URL HERE>>',
+//       'album_id': 9,
+//       'artist_id': 6
+//     }
+//   ])
+//   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
 
-export default PlayerContainer;
+
+//   return (
+//     <Player
+//     currentTrackIndex={currentTrackIndex}
+//     setCurrentTrackIndex={setCurrentTrackIndex}
+//     tracks={tracks}
+//     />
+//   )
+// }
+
+// let style = {
+//   position: "absolute",
+//   bottom: "0",
+//   left: "0",
+//   width: "100%",
+//   height: "80px",
+//   background: "#282828",
+//   // zIndex: "99",
+//   padding: "0 20px",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+// }
+
+export default useAudio;
