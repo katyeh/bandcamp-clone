a0cd7d73346db2fb7b55b32ac51181b4f756cbc7
Yuka <yukamoribe@outlook.com>
2020-11-28 16:00:35 -0500

    Modift scroll arrow to only display if they are scrollable.

--- react-app/src/components/home/Section.js
+++ react-app/src/components/home/Section.js
@@ -1,26 +1,44 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Track from './Track';
-import {useSpring, animated} from 'react-spring';
+import {useSpring, animated, config} from 'react-spring';
 
 
 const Section = ({title, subtitle, tracks}) => {
   const [tracksLoaded, setTracksLoaded] = useState(false);
   const carousel = useRef();
+  const arrowLeft = useRef();
+  const arrowRight = useRef();
+  const [scroll, setScroll] = useState(0);
+  const moveScroll = 220;
 
-  const props = useSpring({ scroll: 100, from: { scroll: 0 } })
+  const [props, set] = useSpring(() => ({
+    scrollLeft: 0,
+    config: config.slow
+  }));
 
   useEffect(() => {
     if (tracks && tracks.length) {
       setTracksLoaded(true);
     }
-  }, [tracks, tracksLoaded])
+  }, [tracks, tracksLoaded]);
+
+  useEffect(() => {
+    arrowLeft.current.style.opacity = scroll === 0 ? 0 : 1;
+    arrowRight.current.style.opacity = scroll === 1540 ? 0 : 1;
+  }, [scroll]);
 
   const handleRightScroll = () => {
-    carousel.current.scrollLeft += 50;
+    if (scroll < carousel.current.scrollWidth - carousel.current.clientWidth) {
+      set({scrollLeft: scroll + moveScroll})
+      setScroll(scroll + moveScroll);
+    }
   }
 
   const handleLeftScroll = () => {
-    carousel.current.scrollLeft -= 50;
+    if (scroll > 0) {
+      set({scrollLeft: scroll - moveScroll})
+      setScroll(scroll - moveScroll)
+    }
   }
 
   return ( 
@@ -28,9 +46,9 @@ const Section = ({title, subtitle, tracks}) => {
       <h2 className="section__title">{title}</h2>
       <h3 className="section__subtitle">{subtitle}</h3>
       <div className="section__contents">
-        <div className="section__contents--right-scroll" onClick={handleRightScroll}>&#9002;</div>
-        <div className="section__contents--left-scroll" onClick={handleLeftScroll}>&#9001;</div>
-        <div ref={carousel} className="section__carousel">
+        <div className="section__contents--right-scroll" ref={arrowRight} onClick={handleRightScroll}>&#9002;</div>
+        <div className="section__contents--left-scroll" ref={arrowLeft} onClick={handleLeftScroll}>&#9001;</div>
+        <animated.div scrollLeft={props.scrollLeft} ref={carousel} className="section__carousel">
           {tracksLoaded && tracks.map(track => (
             <Track key={track.id}>
               <div className="track__image">
@@ -40,7 +58,7 @@ const Section = ({title, subtitle, tracks}) => {
               <p className="track__album-title">{track.album.title}</p>
             </Track>
           ))}
-        </div>
+        </animated.div>
       </div>
     </section>
   );
