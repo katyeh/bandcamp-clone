ebfa27007f3343aa8e0982d65e698fe9d763163b
Yuka <yukamoribe@outlook.com>
2020-11-27 23:30:27 -0500

    Start working on scrolling feature for Section component.

--- react-app/src/components/home/Section.js
+++ react-app/src/components/home/Section.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Track from './Track';
+import {useSpring, animated} from 'react-spring';
+
 
 const Section = ({title, subtitle, tracks}) => {
+  const [toggle, setToggle] = useState(false);
+  const carousel = useRef();
+
+  const props = useSpring({ scroll: 100, from: { scroll: 0 } })
+
+  useEffect(() => {
+  carousel.current.scrollLeft += 80;
+  }, [toggle])
+
   return ( 
     <section className="section">
       <h2 className="section__title">{title}</h2>
       <h3 className="section__subtitle">{subtitle}</h3>
-      <div className="section__contents">
-        {tracks.map(track => (
+      <div ref={carousel} className="section__contents">
+      {tracks.map(track => (
           <Track key={track.id}>
             <div className="track__image">
+              <img src={track.album.album_art_url} />
             </div>
+            <p>{track.title}</p>
+            <p onClick={e => setToggle(!toggle)}>{track.album.title}</p>
           </Track>
         ))}
       </div>
