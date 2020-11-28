d09e21718a5546116bcaf46d992205c878a555e0
Yuka <yukamoribe@outlook.com>
2020-11-28 13:24:52 -0500

    Fix bug on scroll and style icons.

--- react-app/src/components/home/Section.js
+++ react-app/src/components/home/Section.js
@@ -4,26 +4,32 @@ import {useSpring, animated} from 'react-spring';
 
 
 const Section = ({title, subtitle, tracks}) => {
-  const [toggle, setToggle] = useState(false);
   const [tracksLoaded, setTracksLoaded] = useState(false);
   const carousel = useRef();
 
   const props = useSpring({ scroll: 100, from: { scroll: 0 } })
 
   useEffect(() => {
-    carousel.current.scrollLeft += 80;
     if (tracks && tracks.length) {
-      setTracksLoaded(true)
+      setTracksLoaded(true);
     }
-  }, [toggle, tracks, tracksLoaded])
+  }, [tracks, tracksLoaded])
+
+  const handleRightScroll = () => {
+    carousel.current.scrollLeft += 50;
+  }
+
+  const handleLeftScroll = () => {
+    carousel.current.scrollLeft -= 50;
+  }
 
   return ( 
     <section className="section">
       <h2 className="section__title">{title}</h2>
       <h3 className="section__subtitle">{subtitle}</h3>
       <div className="section__contents">
-        <div className="section__contents--left-scroll"></div>
-        <div className="section__contents--right-scroll"></div>
+        <div className="section__contents--right-scroll" onClick={handleRightScroll}>&#9002;</div>
+        <div className="section__contents--left-scroll" onClick={handleLeftScroll}>&#9001;</div>
         <div ref={carousel} className="section__carousel">
           {tracksLoaded && tracks.map(track => (
             <Track key={track.id}>
@@ -31,7 +37,7 @@ const Section = ({title, subtitle, tracks}) => {
                 <img src={track.album.album_art_url} />
               </div>
               <p className="track__title">{track.title}</p>
-              <p onClick={e => setToggle(!toggle)} className="track__album-title">{track.album.title}</p>
+              <p className="track__album-title">{track.album.title}</p>
             </Track>
           ))}
         </div>
