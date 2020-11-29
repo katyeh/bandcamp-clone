f57cd00e1a51318a3ef214039e38afcad58be89f
Kathleen Yeh <kathleenyeh1@gmail.com>
2020-11-28 16:37:04 -0800

    Category bar

--- react-app/src/components/splash/Splash.js
+++ react-app/src/components/splash/Splash.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchBar from './SearchBar';
+import { useDispatch, useSelector } from 'react-redux';
 import FeaturedTracks from './FeaturedTracks';
+import Section from '../home/Section';
+import { getTracks } from '../../store/actions/trackActions';
 
+const Splash = ({ tracks }) => {
+  const [activeClass, setActiveClass] = useState({black: false});
 
-const Splash = () => {
+  const changeColor = () => {
+    setActiveClass({black: true})
+  }
   /* Should render:
     header(with signin/create account buttons),
     banner
@@ -15,18 +22,43 @@ const Splash = () => {
     component featured artists
     component footer with all y'alls info
   */
-  return (
-    <div>
+ let btn_class = activeClass.black ? "blackButton" : "blueButton"
+ return (
+   <div>
       <SearchBar />
-      <FeaturedTracks />
+      {/* <FeaturedTracks /> */}
+      <div className="featured__container">
+        <div className="featured__div">
+          <Section tracks={tracks} title="Featured Tracks"/>
+        </div>
+      </div>
+      <div className="categories__container">
+        <span className={btn_class} onClick={changeColor}>electronic</span>
+        <span>rock</span>
+        <span>metal</span>
+        <span>hip-hop</span>
+        <span>punk</span>
+        <span>jazz</span>
+        <span>classic</span>
+        <span>acoustic</span>
+        <span>pop</span>
+        <span>folk</span>
+      </div>
     </div>
   )
 }
 
 const SplashContainer = () => {
+  const dispatch = useDispatch();
+  const tracks = useSelector(state => state.tracks);
+
+  useEffect(() => {
+    dispatch(getTracks());
+  }, []);
   return (
-    <Splash
-    />
+    <div>
+      <Splash tracks={tracks} />
+    </div>
   )
 }
 
