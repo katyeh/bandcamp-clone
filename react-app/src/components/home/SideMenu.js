cce9a4ccbc5ac3c2d6d703ac2723d4358105979b
Yuka <yukamoribe@outlook.com>
2020-11-29 12:14:04 -0500

    Style some more sidebar.

--- react-app/src/components/home/SideMenu.js
+++ react-app/src/components/home/SideMenu.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const SideMenu = ({artists}) => {
   return ( 
     <div className="sidemenu">
       <div className="sidemenu__based-on-likes">
-        <h5>Artists whom you liked their tracks</h5>
+        <h5 className="sidemenu__title">Artists whom you liked their tracks</h5>
         {artists.based_on_likes && artists.based_on_likes.map(artist => (
-          <div className="sidemenu__container">
-            <img src={`${artist.profile_image_url}`} />
-            <div>{artist.name}</div>
-          </div>
+          <NavLink to={`artists/${artist.id}`}>
+            <div className="sidemenu__container">
+              <img src={`${artist.profile_image_url}`} />
+              <div className="sidemenu__artist-info">
+                <div className="sidemenu__artist-name">{artist.name}</div>
+                <div className="sidemenu__artist-username">{artist.username}</div>
+              </div>
+            </div>
+          </NavLink>
         ))}
       </div>
     </div>
