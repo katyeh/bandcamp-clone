ff356970badfbdcfa7108a090ae685ecb338b714
aleksandar <aca.djordjevic1113@gmail.com>
2020-11-30 08:27:33 -0600

    ale

--- react-app/src/components/artistProfile/ArtistProfile.js
+++ react-app/src/components/artistProfile/ArtistProfile.js
@@ -5,53 +5,72 @@ import { getOneArtist } from "../../store/actions/currentArtist"
 import Albums from './CurrentAlbums'
 import CurrentTracks from './CurrentTracks'
 import ArtistFollowers from './ArtistFollowers'
+import TipModal from './TipModal'
+import './ArtistProfile.css'
 
-function ArtistProfile({ getOneArtist, artist}) { 
+
+function ArtistProfile({ getOneArtist, artist, user}) { 
+  let userProfile = false;
   const { id }  = useParams();
   const artistId = Number.parseInt(id);
   useEffect(() => {
     getOneArtist(artistId)
   }, [artistId])
 
+  if (user.id === artistId) {
+    userProfile = true
+  }
   if (!artist) {
     return null;
   }
 
   return (
     <>
-     <div className="cover_imgage_container">
-      <img src={artist.cover_image_url} alt={artist.cover_image_url} className="cover_image" />
-      <img src={artist.profile_image_url} alt={artist.profile_image_url} className="profile_image" />
-    </div>
-    <div>
-      <CurrentTracks />
-    </div>
-    <div>
-      <Albums/>
-    </div>
-    <div>
-      <ArtistFollowers />
-    </div>
-       <ul>
-         <li>
-          <strong>Artist Id</strong> {artistId}
-        </li>
-        <li>
-          <strong>Artistname</strong> {artist.name}
-        </li>
-        <li>
-          <strong>Bio</strong> {artist.bio}
-        </li>
-        <li>
-          <strong>Country</strong> {artist.country}
-        </li>
-        <li>
-          <strong>City</strong> {artist.city}
-        </li>
-        <li>
-          <strong>Username</strong> {artist.username}
-        </li>
-      </ul>
+    <div className="grid-container">
+        {!userProfile ? (
+          <TipModal />
+        ) : (
+            <div></div>
+          )}
+      <div className="cover_imgage_container">
+        <img src={artist.cover_image_url} alt={artist.cover_image_url} className="cover_image" /> 
+        <img src={artist.profile_image_url} alt={artist.profile_image_url} className="profile_image" />
+      </div>
+          
+      {/* <div>
+        <CurrentTracks />
+      </div> */}
+      <div className="albums-tracks-container">
+        <div>
+          <Albums/>
+        </div>
+        <div>
+          <ArtistFollowers />
+        </div>
+      </div>
+      <div className="artist-info-container">
+        <ul>
+          <li>
+            <strong>Artist Id</strong> {artistId}
+          </li>
+          <li>
+            <strong>Artistname</strong> {artist.name}
+          </li>
+          <li>
+            <strong>Bio</strong> {artist.bio}
+          </li>
+          <li>
+            <strong>Country</strong> {artist.country}
+          </li>
+          <li>
+            <strong>City</strong> {artist.city}
+          </li>
+          <li>
+            <strong>Username</strong> {artist.username}
+          </li>
+        </ul>
+      </div>
+      </div>
     </>
   );
 }
@@ -60,13 +79,17 @@ function ArtistProfile({ getOneArtist, artist}) {
 const ArtistProfileContainer = () => {
 
   const artist = useSelector((state) => state.currentArtist);
+  const user = useSelector((state) => state.user)
   const dispatch = useDispatch()
   return (
     <ArtistProfile
       artist={artist}
       getOneArtist={(id) => dispatch(getOneArtist(id))}
+      user={user}
     />
   );
 }
 
 export default ArtistProfileContainer;
+
+
---
+++ react-app/src/components/artistProfile/TipModal.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Modal from "react-modal";
+import './TipModal.css'
+
+
+Modal.setAppElement('#root');
+
+const TipModal = () => {
+    // const [errors, setErrors] = useState([]);
+    const [amount, setAmount] = useState("0");
+    
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    let history = useHistory();
+
+    const updateAmount = (e) => {
+        setAmount(e.target.value);
+    };
+
+
+
+    // onSubmit = { onTip }
+    // const onTip = async (e) => {
+    //     e.preventDefault();
+    //     const 
+    // }
+
+    // const onLogin = async (e) => {
+    //     e.preventDefault();
+    //     const user = await login(email, password);
+    //     if (!user.errors) {
+    //         setAuthenticated(true);
+    //         setIsOpen(false);
+    //         history.push("/")
+    //     } else {
+    //         setErrors(user.errors);
+    //     }
+    // };
+
+    return (
+        <div>
+            <button className="signup__btn" onClick={() => setIsOpen(true)}>Tip</button>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={() => setIsOpen(false)}
+                contentLabel="Tip Modal"
+                className="login-modal"
+                overlayClassName="overlay"
+                shouldCloseOnOverlayClick={true}
+            >
+                <div className="login-header">
+                    <h2>Tip</h2>
+                    <button className="close-btn" onClick={() => setIsOpen(false)}>X</button>
+                </div>
+
+                <form >
+                    <div className="login-content">
+                        <label for="points">Choose Amount</label>
+                        <input
+                            className="slider"
+                            type="range"
+                            name="points" 
+                            min="5" 
+                            step="5"
+                            max="30"
+                            onChange={updateAmount}
+                            value={amount}
+                        ></input>
+                    </div>
+                    <div className="login-content">
+                        Amount: {amount} 
+                    </div>
+                    <div>
+                        <image src="./dough2.png"></image>
+                    </div>
+                        <div className="login-content">
+                            <button className="login-btn" type="submit">TIP</button>
+                        </div>
+                </form>
+            </Modal>
+        </div>
+    )
+}
+
+export default TipModal
