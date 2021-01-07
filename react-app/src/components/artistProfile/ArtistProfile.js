befcc24525aeeb1ea41ae12e42bc42d29ffdb043
aleksandar <aca.djordjevic1113@gmail.com>
2021-01-06 23:14:06 -0800

    uploading cover and profile image works but needs styling

--- react-app/src/components/artistProfile/ArtistProfile.js
+++ react-app/src/components/artistProfile/ArtistProfile.js
@@ -9,10 +9,10 @@ import TipModal from './TipModal'
 import UploadAlbum from './UploadAlbum'
 import { getAlbums } from '../../store/actions/albumActions'
 import UploadCoverPic from './UploadCoverPic'
+import UploadProfilePic from './UploadProfilePic'
 
 
 function ArtistProfile({ getOneArtist, artist, user, albums, getAlbums}) {
-  const dispatch = useDispatch();
   const [trackDisplay, setTrackDisplay] = useState(false);
   const [albumDisplay, setAlbumDisplay] = useState(true);
   const [albumClass, setAlbumClass] = useState(true);
@@ -71,10 +71,12 @@ function ArtistProfile({ getOneArtist, artist, user, albums, getAlbums}) {
         >
           {/* <img src={artist.cover_image_url} alt={artist.cover_image_url} className="cover_image" />  */}
           <img src={artist.profile_image_url} alt={artist.profile_image_url} className="profile_image" />
+          <UploadProfilePic user={user} />
           <div className="artist-name">
             <h1>{artist.username}</h1>
           </div>
         </div>
+        <UploadCoverPic user={user}/>
         <div className="profile__body">
           <div className="albums-tracks__container">
             <div className="album-track__btns">
