ec32c40c59d0033ea6d6d4200178158e33e824f0
Yuka <yukamoribe@outlook.com>
2021-01-12 16:23:05 -0500

    Reorganize header component so that navbar remains even after image slide.

--- react-app/src/components/splash/Header.js
+++ react-app/src/components/splash/Header.js
@@ -8,45 +8,17 @@ import headerImage1 from "../../assets/busker_header.jpeg";
 import headerImage2 from "../../assets/busker_header2.jpeg";
 import headerImage3 from "../../assets/busker_header3.jpeg";
 import AwesomeSlider from 'react-awesome-slider';
-import AwsSliderStyles from 'react-awesome-slider/src/styles.js';
+import _AwsSliderStyles from 'react-awesome-slider/src/styles.js';
 
 const Header = ( {authenticated, setAuthenticated} ) => {
-
-  const startupScreen = (
-    <div>
-      <img src={headerImage1} alt="header image 1"/>
-    </div>
-  );
-  
-  const slider = (
-    <AwesomeSlider
-      startupScreen={startupScreen}
-      // cssModule={styles}
-    >
-    
-    </AwesomeSlider>
-  );
-
-  // const slider = (
-  //   <AwesomeSlider animation="cubeAnimation">
-  //     <div>
-  //   <img src="./busker.png"/>
-  // </div>
-  //     {/* <div data-src="../../assets/busker_logo2.png" /> */}
-  //   </AwesomeSlider>
-  // );
-
   return (
-    <AwesomeSlider>
-      <div id="header__image1" data-src={headerImage1} alt="header image" />
-      <div id="header__image2" data-src={headerImage2} alt="header image" />
-      <div id="header__image3" data-src={headerImage3} alt="header image" />
+    <>
       <div className="header__container">
         <NavLink to='/splash'>
-        <div className="header__logo">
-          <img alt="" className="header__img1" src={logo1}></img>
-          <img alt="" className="header__img2" src={logo2}></img>
-        </div>
+          <div className="header__logo">
+            <img alt="" className="header__img1" src={logo1}></img>
+            <img alt="" className="header__img2" src={logo2}></img>
+          </div>
         </NavLink>
         <div className="header-btn__container">
           <div className="header-btn__wrapper">
@@ -63,7 +35,13 @@ const Header = ( {authenticated, setAuthenticated} ) => {
           </div>
         </div>
       </div>
-     </AwesomeSlider>
+
+      <AwesomeSlider>
+        <div id="header__image1" data-src={headerImage1} alt="header image" />
+        <div id="header__image2" data-src={headerImage2} alt="header image" />
+        <div id="header__image3" data-src={headerImage3} alt="header image" />
+      </AwesomeSlider>
+    </>
   )
 }
 
