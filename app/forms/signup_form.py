5b6bb29b5e997887adab0ba83422f833b4e3a68b
Yuka <yukamoribe@outlook.com>
2021-01-17 11:11:32 -0500

    Add WTF validation on signup for when there is already an user with given username.

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -3,20 +3,29 @@ from wtforms import StringField, FileField
 from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
 from app.models import Artist
 
+def user_exists_with_username(form, field):
+    print("Checking if user exits", field.data)
+    username = field.data
+    user = Artist.query.filter(Artist.username == username).first()
+    if user:
+        raise ValidationError("There is already an user with given username")
 
-def user_exists(form, field):
+
+def user_exists_with_email(form, field):
     print("Checking if user exits", field.data)
     email = field.data
     user = Artist.query.filter(Artist.email == email).first()
     if user:
-        raise ValidationError("There is already an user with registered email")
+        raise ValidationError("There is already an user with given email")
 
 class SignUpForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
-    username = StringField('username', validators=[DataRequired()])
+    username = StringField('username', validators=[
+               DataRequired(),
+               user_exists_with_username])
     email = StringField('email', validators=[DataRequired(),
             Email('Please enter a valid email'),
-            user_exists])
+            user_exists_with_email])
     password = StringField('password', validators=[DataRequired()])
     repeat_password = StringField('repeat_password', validators=[
                       DataRequired(),
